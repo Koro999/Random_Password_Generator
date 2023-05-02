a104a501cadea2da61fd4239f3a04f44369566bf
Koro999 <chris.dang93@hotmail.com>
2023-05-02 19:41:34 -0400

    User prompts done, user validation done

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
 // Assignment code here
-var passwordLength, passwordLowercase, passwordUppercase ; 
+var passwordLengthChoice, passwordLowercaseChoice, passwordUppercaseChoice, passwordNumberChoice, passwordSpecialChoice; 
 
-//Arrays to store number and special characters. All array lengths include 0
+//Arrays to store number and special characters. All array.lengths include 0
+
+const passwordLength = [];
 
 const passwordNumbers = ["0","1","2","3","4","5","6","7","8","9",];//array.length == 10 
 
@@ -15,10 +17,31 @@ const passwordSpecial = ["!", " \" " , ",", "#", "$", "%", "&","\'", "(" ,")",
 
 
 function userPrompts () {
+  passwordLengthChoice = prompt("How many characters do you want the password to be?");
+  var passwordLengthChoiceConvert = Number(passwordLengthChoice); //convert password length from a string to a number for later us
+  var passwordLengthCheck = isNaN(passwordLengthChoiceConvert);
   
+  var valid = false;
+  while (valid ==false){ //checks the entry of the box to make sure the entry is valid
+      if (passwordLengthChoiceConvert > 128 || passwordLengthChoiceConvert < 8  || passwordLengthCheck === true) {
+        passwordLengthChoice = prompt("Please enter a numeric value between 8 and 128!");
+        passwordLengthChoiceConvert = Number(passwordLengthChoice);
+        passwordLengthCheck = isNaN(passwordLengthChoiceConvert);
+      }
+      else{
+        valid=true;
+        return;
+      }        
+    }
+  
+  passwordLowercaseChoice = confirm("Do you want to include lower-case letters?");
+  passwordUppercaseChoice = confirm("Do you want to include upper-case letters?");
+  passwordNumberChoice = confirm("Do you want to include numbers?");
+  passwordSpecialChoice = confirm("Do you want to include special characters?");
+  return;
 }
 
-
+userPrompts ();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
