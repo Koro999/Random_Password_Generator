208ea5b4f5a65b651d89231dd6f0f2b1558580c5
Koro999 <chris.dang93@hotmail.com>
2023-05-02 18:14:48 -0400

    main variables defined and assigned into arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,23 @@
 // Assignment code here
+var passwordLength, passwordLowercase, passwordUppercase ; 
+
+//Arrays to store number and special characters. All array lengths include 0
+
+const passwordNumbers = ["0","1","2","3","4","5","6","7","8","9",];//array.length == 10 
+
+const passwordLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                         "k","l", "m", "n", "o", "p", "q", "r", "s", "t",
+                         "u", "v", "w", "x","y","z",];//array.length == 26 
+
+const passwordSpecial = ["!", " \" " , ",", "#", "$", "%", "&","\'", "(" ,")",
+                         "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">",
+                         "?", "@", "[", " \ ", "]", "^", "_", "\`", "{" , "|", "}", "~"]; //array.length == 33 
+
+
+function userPrompts () {
+  
+}
+
 
 
 // Get references to the #generate element
